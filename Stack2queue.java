c3550412c75313df4bfb1e5dddca9492fe0956e8
nisha1992 <nisha.dvi9@gmail.com>
2014-07-02 16:19:08 +0530

    Update Stack2queue.java

--- Stack2queue.java
+++ Stack2queue.java
@@ -1,22 +1,22 @@
 class Stack2queue
 {
-public static void main(String arg[])
-{
-stackImp<Integer> o1=new stackImp();
-stackImp<Integer> o2=new stackImp();
-o1.push(1);
-o1.push(2);
-o1.push(3);
-o1.push(4);
-o1.push(5);
-o2.push(o1.pop());
-o2.push(o1.pop());
-o2.push(o1.pop());
-o2.push(o1.pop());
-o2.push(o1.pop());
-while(o2.getTop()!=null)
-{
-System.out.println(o2.pop());
-}
-}
+  public static void main(String arg[])
+  {
+    stackImp<Integer> o1=new stackImp();
+    stackImp<Integer> o2=new stackImp();
+    o1.push(1);
+    o1.push(2);
+    o1.push(3);
+    o1.push(4);
+    o1.push(5);
+    o2.push(o1.pop());
+    o2.push(o1.pop());
+    o2.push(o1.pop());
+    o2.push(o1.pop());
+    o2.push(o1.pop());
+    while(o2.getTop()!=null)
+    {
+      System.out.println(o2.pop());
+    }
+  }
 }
